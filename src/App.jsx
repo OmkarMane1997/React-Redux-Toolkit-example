4843a85e2330aa290bfa2e1357019dfedb845cd6
OmkarMane1997 <omii0.mane@gmail.com>
2025-02-11 16:45:55 +0530

    bootstrap add in the project and UI make the changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,35 @@ import {
   increment,
   incrementByAmount,
 } from '../src/features/counterSlice';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  
   return (
-    <>
-      <h1>Resux tool kit with React</h1>
-      <p> counter value is {count}</p>
-      <button type="button" onClick={() => dispatch(increment())}>
-        incrment
-      </button>
-      <br />
-      <button type="button" onClick={() => dispatch(decrement())}>
-        decremnt
-      </button>
-      <br />
+    <div className="d-flex justify-content-center align-items-center vh-100"> {/* Centered card container */}
+      <div className="card p-4 shadow text-center"> {/* Bootstrap Card */}
+        <h1>Redux Toolkit with React</h1>
+        <p className="fs-4">Counter value is {count}</p>
+        
+        {/* Increment Button */}
+        <button className="btn btn-primary m-2" type="button" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        
+        {/* Decrement Button */}
+        <button className="btn btn-danger m-2" type="button" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
 
-      <button type="button" onClick={() => dispatch(incrementByAmount(5))}>
-        incrment by amount
-      </button>
-    </>
+        {/* Increment by Amount Button */}
+        <button className="btn btn-success m-2" type="button" onClick={() => dispatch(incrementByAmount(5))}>
+          Increment by Amount
+        </button>
+      </div>
+    </div>
   );
 }
 
